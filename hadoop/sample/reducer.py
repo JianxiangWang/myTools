f211cb9f532f06a0acb4e4306e027d53447867b3
JianxiangWang <w51141201062@163.com>
2016-08-03 21:55:41 +0800

    hi~

--- hadoop/sample/reducer.py
+++ hadoop/sample/reducer.py
@@ -1,14 +1,18 @@
 #!python/bin/python
-# rand_subset_r.py
-
 import sys
 
-k = int(sys.argv[1])
-c = 0
 
-for line in sys.stdin:
-    (r, x) = line.strip().split('\t', 1)
-    print x
-    c += 1
-    if c == k:
-        break
+def main():
+    k = int(sys.argv[1])
+    c = 0
+
+    for line in sys.stdin:
+        (r, x) = line.strip().split('\t', 1)
+        print x
+        c += 1
+        if c == k:
+            break
+
+
+if __name__ == '__main__':
+    main()
